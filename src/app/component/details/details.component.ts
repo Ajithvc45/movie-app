af42e4e142192abbd5fea42ae7e394f850d50baa
Ajithvc45 <ajithvc45@gmail.com>
2022-10-10 16:15:04 +0530

    route with id done

--- src/app/component/details/details.component.ts
+++ src/app/component/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/movies.service';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Movie } from 'Movie';
 
 @Component({
@@ -12,14 +13,20 @@ export class DetailsComponent implements OnInit {
 
   // movieData = new Movie('','')
   movieData: any = [];
+  id: any;
 
-  constructor(private movieService:MoviesService, private router:Router) { }
+  constructor(private movieService:MoviesService, private router:Router, private active:ActivatedRoute) { }
 
   ngOnInit(): void {
-    let id = localStorage.getItem('details');
-    console.log('data got with id'+id);
-    this.movieService.singleMovie(id).subscribe((data)=>{
-    this.movieData=JSON.parse(JSON.stringify(data));
+    // let id = localStorage.getItem(`details/${this.movieData.id}`);
+    // console.log('data got with id'+ id);
+    // this.movieService.singleMovie(id).subscribe((data)=>{
+    // this.movieData=JSON.parse(JSON.stringify(data));
+    // })
+    this.id = this.active.snapshot.params['id']
+    console.log("movie id is"+ this.id);
+    this.movieService.singleMovie(this.id).subscribe((data)=>{
+      this.movieData=JSON.parse(JSON.stringify(data));
     })
   }
 
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movie } from 'Movie';
 import { MoviesService } from 'src/app/movies.service';
 import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
   movies: any = [];
   full_path: any;
   poster_path:any;
-  
+  id = " "
+
   ngOnInit(): void {
     this.movieService.getMovies().subscribe((movie) => {
       this.movies = Object.values(movie)
@@ -23,12 +24,20 @@ export class HomeComponent implements OnInit {
       // this.full_path = "https://image.tmdb.org/t/p/w500/" + this.poster_path;
       console.log(this.movies);
     });
+
+    this.active.paramMap.subscribe((params: ParamMap) => {
+      console.log('---------------- ', params.get('id'));
+    })
   }
 
   showMovie(movie:any) {
-    localStorage.setItem("details", movie.id.toString());
-    this.router.navigate(['details']);
-    console.log(movie.id);
+    // localStorage.setItem("details", movie.id.toString());
+    // this.router.navigate(['details']);
+    console.log('movie.id',movie.id);
+    // this.id = this.active.snapshot.params['id']
+    
+    // console.log(this.id)
+    this.router.navigate([`/details/${movie.id}`])
   }
 
 }
