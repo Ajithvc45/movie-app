f2fe0a1fa6f4231da60fd131e6481c8bc8ccbef4
Ajithvc45 <ajithvc45@gmail.com>
2022-10-11 13:22:33 +0530

    modal video done

--- src/app/component/details/details.component.ts
+++ src/app/component/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/movies.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Movie } from 'Movie';
 
 @Component({
@@ -14,8 +15,15 @@ export class DetailsComponent implements OnInit {
   // movieData = new Movie('','')
   movieData: any = [];
   id: any;
+  controllerSrc: any;
+  movieTrailer:any;
 
-  constructor(private movieService:MoviesService, private router:Router, private active:ActivatedRoute) { }
+  constructor(
+    private movieService:MoviesService, 
+    private router:Router, 
+    private active:ActivatedRoute, 
+    private sanitizer:DomSanitizer
+    ) { }
 
   ngOnInit(): void {
     // let id = localStorage.getItem(`details/${this.movieData.id}`);
@@ -27,12 +35,38 @@ export class DetailsComponent implements OnInit {
     console.log("movie id is"+ this.id);
     this.movieService.singleMovie(this.id).subscribe((data)=>{
     this.movieData=JSON.parse(JSON.stringify(data));
+
+    this.movieService.getVideo(this.id).subscribe((movie) => {
+    this.movieTrailer=JSON.parse(JSON.stringify(movie));
+    console.log(this.movieTrailer.results[0].key);
+    const url=`https://www.youtube.com/embed/${this.movieTrailer.results[0].key}`;
+    this.controllerSrc = this.sanitizer.bypassSecurityTrustResourceUrl(url);
     })
+    
+    // console.log(this.movie);
+    // console.log(this.movieData);
+    });
+
+    // this.movie = this.active.snapshot.params['movie']
+    // this.movieService.getVideo(this.id).subscribe((movie) => {
+    // this.movieData = Object.values(movie)
+    // console.log(this.movieData)
+    // const url=`https://www.youtube.com/embed/${this.movieData}`;
+    // this.controllerSrc = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    // this.movie = this.id
+    // this.movieService.getVideo(this.id).subscribe((data)=> {
+    // this.movieData = JSON.parse(JSON.stringify(data));
+    
+    // })
   }
 
   backward() {
     localStorage.removeItem('details')
     this.router.navigate(['']);
+  }
+
+  getVideo() {
+    console.log("helloo")
   }
 
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -20,11 +20,11 @@ export class MoviesService {
   }
 
   searchedMovie(movie:any): Observable<Movie[]> {
-    console.log(movie);
     return this.http.get<Movie[]>(`https://api.themoviedb.org/3/search/movie?api_key=2c7d2e78869ec86ee78d10bc69e698c9&language=en-US&query=${movie}&page=1&include_adult=false`);
   }
 
   getVideo(id:any): Observable<Movie[]> {
+    console.log(id)
     return this.http.get<Movie[]>(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=2c7d2e78869ec86ee78d10bc69e698c9&language=en-US`);
   }
 }
